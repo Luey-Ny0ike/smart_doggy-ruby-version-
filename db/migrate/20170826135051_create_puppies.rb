2a551844afc183676ed509c06b1d2157bbbb36bc
lewis nyoike <nyoikelewis@gmail.com>
2017-08-26 17:05:12 +0300

    Fixed the migrate issues

--- db/migrate/20170826135051_create_puppies.rb
+++ db/migrate/20170826135051_create_puppies.rb
@@ -2,13 +2,13 @@ class CreatePuppies < ActiveRecord::Migration[5.1]
   def change
     create_table :puppies do |t|
       t.string :name
-      t.int :age
+      t.integer :age
       t.string :breed
       t.string :gender
       t.string :color
       t.string :nickname
       t.string :title
-      t.varchar :price
+      t.integer :price
       t.string :recomendation
       t.string :description
 
