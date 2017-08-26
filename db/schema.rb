f67218f0d0bd23f8bf636ffbd7d394311bde5588
lewis nyoike <nyoikelewis@gmail.com>
2017-08-26 17:16:22 +0300

    Migrated into the testimonials table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170826140327) do
+ActiveRecord::Schema.define(version: 20170826141119) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,13 @@ ActiveRecord::Schema.define(version: 20170826140327) do
     t.integer "price"
     t.string "recomendation"
     t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "testimonials", force: :cascade do |t|
+    t.string "title"
+    t.string "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
