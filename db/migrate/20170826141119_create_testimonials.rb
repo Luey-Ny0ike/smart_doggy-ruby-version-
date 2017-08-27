0439c8eb53013a4e8382767c903f9de8f19f77a3
lewis nyoike <nyoikelewis@gmail.com>
2017-08-27 15:18:19 +0300

    Changed the data type for the message

--- db/migrate/20170826141119_create_testimonials.rb
+++ db/migrate/20170826141119_create_testimonials.rb
@@ -2,7 +2,7 @@ class CreateTestimonials < ActiveRecord::Migration[5.1]
   def change
     create_table :testimonials do |t|
       t.string :title
-      t.column :message, :varchar
+      t.column :message, :string
 
       t.timestamps
     end
