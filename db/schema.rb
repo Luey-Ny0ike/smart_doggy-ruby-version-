4225961b8706ca0fe1659888f2e201680f8c34f2
lewis nyoike <nyoikelewis@gmail.com>
2017-08-27 21:36:42 +0300

    migrated the table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170826205634) do
+ActiveRecord::Schema.define(version: 20170827180223) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,11 @@ ActiveRecord::Schema.define(version: 20170826205634) do
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "home", force: :cascade do |t|
+    t.integer "dog_id"
+    t.integer "puppy_id"
   end
 
   create_table "puppies", force: :cascade do |t|
