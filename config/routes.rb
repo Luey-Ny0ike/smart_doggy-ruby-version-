11c7453c1988e129c65dec419b11dfe55b6b3ea8
lewis nyoike <nyoikelewis@gmail.com>
2017-08-28 16:13:57 +0300

    Mounted the rails_admin and deleted the home resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users, :controllers => { :registrations => "users/registrations"}
   root to: 'home#index'
   resources :testimonials do
@@ -7,6 +8,5 @@ Rails.application.routes.draw do
   resources :tricks
   resources :puppies
   resources :dogs
-  resources :home
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
