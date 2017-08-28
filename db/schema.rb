8ae4d3d283fb6c9e0afa5b33b9843b27bff9abbe
lewis nyoike <nyoikelewis@gmail.com>
2017-08-28 11:43:46 +0300

    migrated the db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170827213418) do
+ActiveRecord::Schema.define(version: 20170828083319) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20170827213418) do
     t.integer "testimonial_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "dogs", force: :cascade do |t|
