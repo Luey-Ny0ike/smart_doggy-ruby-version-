51cfce2e5c2ad622b83bbb9384309edf6483840e
lewis nyoike <nyoikelewis@gmail.com>
2017-08-28 12:02:02 +0300

    migrated the db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170828083319) do
+ActiveRecord::Schema.define(version: 20170828084415) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 20170828083319) do
     t.string "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "tricks", force: :cascade do |t|
