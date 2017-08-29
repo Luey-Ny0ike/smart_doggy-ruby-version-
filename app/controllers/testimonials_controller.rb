0a5060e2f0bb7a630959e275a96be85a9b1b9e99
lewis nyoike <nyoikelewis@gmail.com>
2017-08-29 14:57:04 +0300

    permited the image

--- app/controllers/testimonials_controller.rb
+++ app/controllers/testimonials_controller.rb
@@ -70,6 +70,6 @@ class TestimonialsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def testimonial_params
-      params.require(:testimonial).permit(:title, :message)
+      params.require(:testimonial).permit(:title, :message, :image)
     end
 end
