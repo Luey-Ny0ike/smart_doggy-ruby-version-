260f01e0e8caa0d6584b1a69d29c3946cf958ca9
lewis nyoike <nyoikelewis@gmail.com>
2017-08-29 14:33:03 +0300

    Attatched the images

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,2 +1,8 @@
 class Dog < ApplicationRecord
+  has_attached_file :image1, styles: { large: "600x 600>", medium: "300x300>", thumb: "150x150#" }
+  validates_attachment_content_type :image1, content_type: /\Aimage\/.*\z/
+  has_attached_file :image2, styles: { large: "600x 600>", medium: "300x300>", thumb: "150x150#" }
+  validates_attachment_content_type :image1, content_type: /\Aimage\/.*\z/
+  has_attached_file :image3, styles: { large: "600x 600>", medium: "300x300>", thumb: "150x150#" }
+  validates_attachment_content_type :image1, content_type: /\Aimage\/.*\z/
 end
