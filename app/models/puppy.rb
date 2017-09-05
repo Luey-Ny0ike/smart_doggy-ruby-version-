bb986f89917a68608d8bfaf21e4db7fccf73857e
lewis nyoike <nyoikelewis@gmail.com>
2017-09-05 20:54:22 +0300

    Added relation between the bookings and puppies

--- app/models/puppy.rb
+++ app/models/puppy.rb
@@ -5,4 +5,5 @@ class Puppy < ApplicationRecord
   validates_attachment_content_type :image1, content_type: /\Aimage\/.*\z/
   has_attached_file :image3, styles: { large: "600x 600>", medium: "300x300>", thumb: "150x150#" }
   validates_attachment_content_type :image1, content_type: /\Aimage\/.*\z/
+  has_many :bookings
 end
