56362fe72a914e988bac64754f0cb26d1b5e1d99
lewis nyoike <nyoikelewis@gmail.com>
2017-09-05 21:14:36 +0300

    Nested the bookings under the puppy resource

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
     resources :comments
   end
   resources :tricks
-  resources :puppies
+  resources :puppies do
+    resources :bookings
+  end
   resources :dogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
