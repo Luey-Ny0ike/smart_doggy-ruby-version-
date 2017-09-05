b62df88844f7f4acea7cba9cbd121087f62ac3d0
lewis nyoike <nyoikelewis@gmail.com>
2017-09-05 20:54:57 +0300

    Defined a method to check presence of a message and title

--- app/models/testimonial.rb
+++ app/models/testimonial.rb
@@ -3,4 +3,6 @@ class Testimonial < ApplicationRecord
   belongs_to :user
   has_attached_file :image, styles: { large: "600x 600>", medium: "300x300>", thumb: "150x150#" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+  validates_presence_of :title
+  validates_presence_of :message
 end
