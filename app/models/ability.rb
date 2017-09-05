2e0bca666a35f2b2aa9d2f48fca3af8a343c2611
lewis nyoike <nyoikelewis@gmail.com>
2017-09-05 20:51:56 +0300

    Added ability to super admin

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -7,6 +7,7 @@ class Ability
     # user ||= User.new # guest user (not logged in)
     if user && user.superadmin_role?
       can :manage, :all
+      can :manage, :user
       can :access, :rails_admin
       can :dashboard
     end
