a898b5103051abe2db11c163e2fea2c0ea315f47
lewis nyoike <nyoikelewis@gmail.com>
2017-09-10 01:06:24 +0300

    Redirect to root_path once logged out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_out_path_for(_user)
-    testimonials_path
+    root_path
   end
 
   def after_sign_in_path_for(_user)
