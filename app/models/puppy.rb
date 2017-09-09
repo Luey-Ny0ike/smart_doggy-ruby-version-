15f6684a629497789ab8a9a70713e8e0788ce5bc
lewis nyoike <nyoikelewis@gmail.com>
2017-09-10 00:41:46 +0300

    Changed the size of images

--- app/models/puppy.rb
+++ app/models/puppy.rb
@@ -1,5 +1,5 @@
 class Puppy < ApplicationRecord
-  has_attached_file :image1, styles: { large: "600x 600>", medium: "300x300>", thumb: "150x150#" }
+  has_attached_file :image1, styles: { large: "600x 600>", medium: "370x370#", thumb: "150x150#" }
   validates_attachment_content_type :image1, content_type: /\Aimage\/.*\z/
   has_attached_file :image2, styles: { large: "600x 600>", medium: "300x300>", thumb: "150x150#" }
   validates_attachment_content_type :image1, content_type: /\Aimage\/.*\z/
