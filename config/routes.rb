3da7d9fa6f8779744035442f0825ef759536a70c
lewis nyoike <nyoikelewis@gmail.com>
2017-09-10 01:54:40 +0300

    Contacts resource

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,6 @@ Rails.application.routes.draw do
     resources :bookings
   end
   resources :dogs
+  resources :contacts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
